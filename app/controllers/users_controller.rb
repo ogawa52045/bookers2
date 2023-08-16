769df16c82cb19913ed98860f98432c2ccc21cbd
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-08-16 15:27:44 +0000

    [finish]bookers2_20230816

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def edit
     @user = User.find(params[:id])
   end
@@ -14,10 +15,20 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
   
+  def index
+    @users = User.all
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 end
