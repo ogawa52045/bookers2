770f353c3add147e1084695b48c9c924b2c674c0
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-08-20 11:21:50 +0000

    [finish]bookers2-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-  sessions: 'custom_sessions'
-  }
+  devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
