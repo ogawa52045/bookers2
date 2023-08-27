d8751578d50b9f3db3c2e9c74607b14725ab78ed
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-08-27 08:15:02 +0000

    [finish]bookers2-edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
    def after_sign_in_path_for(resource)
-     if current_user.first_sign_in?
-      flash[:notive]= 'Welcome! You have signed up successfully.'
-      user_path(current_user.id)
-     else
-      flash[:notice]= 'Signed in successfully.'
-      user_path(current_user.id)
-     end
+     user_path(current_user.id)
    end
    
    def after_sign_out_path_for(resource)
