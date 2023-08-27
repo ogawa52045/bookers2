9ef7e229f66c39a745d514e89cc0c6510b34ca4a
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-08-27 16:07:54 +0000

    [finish]bookers2-error-edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def new
     @user = current_user
     @book = Book.new
@@ -53,6 +54,13 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+    def is_matching_login_user
+    user = User.find(params[:id])
+     unless user.id == current_user.id
+      redirect_to books_path
+     end
+    end
   
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
